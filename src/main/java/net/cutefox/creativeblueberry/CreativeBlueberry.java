aa5fade08accf14bcc4ab2557b7c7331f8b1f485
CuteFox8302 <111240985+CuteFox8302@users.noreply.github.com>
2023-01-16 11:42:57 -0500

    Initial commit.

---
+++ src/main/java/net/cutefox/creativeblueberry/CreativeBlueberry.java
@@ -0,0 +1,15 @@
+package net.cutefox.creativeblueberry;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreativeBlueberry implements ModInitializer {
+    public static final String MOD_ID = "assets/creativeblueberry";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/cutefox/creativeblueberry/CreativeBlueberryClient.java
@@ -0,0 +1,10 @@
+package net.cutefox.creativeblueberry;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreativeBlueberryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
